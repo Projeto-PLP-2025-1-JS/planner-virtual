1493c1f4051fdf8141992f3738a8aeb7af682e27
Renato Beltrão <renatinbeltrao@gmail.com>
2025-08-22 12:04:45 -0300

    Fazendo metodos create e getMetas em metaController

--- server/controller/metaController.js
+++ server/controller/metaController.js
@@ -1,16 +1,30 @@
-import {metaService} from './service/metaService.js'
+
+import {metaService} from '../service/metaService.js'
 
 export const metaController = {
 
-    criar: (request,reply) => {
+    create: (request,reply) => {
         try{
-            metaService.criarMeta(request.body)
+            metaService.create(request.body)
             return reply.status(201).send()
         }
         catch(e){
-            return reply.status(400).send({ message: 'Erro na validação de meta.', errors: error.issues })
+            return reply.status(400).send(e.message)
+            
         }
+        
         //fim do metodo de criar
     },
+
+    getMetas:(request,reply) => {
+        try{
+            const {search} = request.query
+            const metas = metaService.getMetas(search)
+            return reply.status(200).send(metas)
+        }catch(e){
+            return reply.status(400).send(e.message)
+        }
     
+},
+
 }
