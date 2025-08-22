0ffc740c6ad0543dcb3f78d1b94b909057254544
Renato Beltrão <renatinbeltrao@gmail.com>
2025-08-22 10:15:29 -0300

    Fazendo metodo criar em metaController

--- server/controller/metaController.js
+++ server/controller/metaController.js
@@ -1,12 +1,16 @@
+import {metaService} from './service/metaService.js'
+
 export const metaController = {
 
     criar: (request,reply) => {
         try{
-
+            metaService.criarMeta(request.body)
+            return reply.status(201).send()
         }
         catch(e){
-            
+            return reply.status(400).send({ message: 'Erro na validação de meta.', errors: error.issues })
         }
+        //fim do metodo de criar
     },
-    //fim do metodo de criar
+    
 }
