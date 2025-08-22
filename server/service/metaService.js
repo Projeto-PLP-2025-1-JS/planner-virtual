976b966bdf067cf59436442e5dff0e1fb5981ed5
Renato Beltrão <renatinbeltrao@gmail.com>
2025-08-22 12:07:09 -0300

    Adicionando metodos create e getMetas e alterando a funcionalidade de database para ele ser unico

--- server/service/metaService.js
+++ server/service/metaService.js
@@ -1,11 +1,24 @@
-import { DatabaseMemory } from "../database/database-memory.js";
-import { metaModel } from "../model/tarefaModel.js";
+import { database } from "../database/database.js";
+import { metaModel } from "../model/metaModel.js";
 
-const database = new DatabaseMemory()
 
 export const metaService = {
-    criar: (metaData) => {
-        const meta = metalModel.parse(metaData);
-        database.create(meta);
+    create: (metaData) => {
+        const meta = metaModel.parse(metaData);
+        try{
+        database.createMeta(meta);}
+        catch (error) {
+            throw new Error('Erro ao criar meta: ' + error.message);
+            console.log(error);
+        }
+    },
+
+    getMetas: (search) => {
+        try{
+            const metas = database.getMetas(search)
+            return metas;
+        }catch (e){
+            throw new Error('Erro ao buscar metas: ' + e.message);
+        }
     },
 }
