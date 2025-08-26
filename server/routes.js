01ae7677036215f456d5ed30d5f3394e86f810d4
Ruan-Alves-Paz <156556770+RuanDiego2004@users.noreply.github.com>
2025-08-26 14:33:08 -0300

    Transformando os objetos de metas em classes

--- server/routes.js
+++ server/routes.js
@@ -2,12 +2,15 @@ import { metaController } from "./controller/metaController.js";
 import { tarefaController } from "./controller/tarefaController.js";
 import { tarefaService } from "./service/tarefa-service.js";
 import { DatabaseMemory } from "./database/database-memory.js";
+import { metaService } from "./service/metaService.js";
 
 export function appRoutes (server){
 
     const database = new DatabaseMemory();
     const tarefaServices = new tarefaService(database);
+    const metaServices = new metaService(database);
     const tarefaControllers = new tarefaController(tarefaServices);
+    const metaControllers = new metaController(metaServices);
 
     // Rotas de tarefa
     server.post('/tarefa/create', tarefaControllers.criar.bind(tarefaControllers));
@@ -16,8 +19,8 @@ export function appRoutes (server){
     server.delete('/tarefa/:id', tarefaControllers.delete.bind(tarefaControllers));
 
     //server.get('/meta/get',metaController.listar)
-    server.post('/meta/create',metaController.create)
-    server.get('/meta/get',metaController.getMetas)
-    server.put('/meta/update/:id',metaController.updateMeta)
-    server.delete('/meta/delete/:id',metaController.deleteMeta)
+    server.post('/meta/create',metaControllers.create.bind(metaControllers));
+    server.get('/meta/get',metaControllers.getMetas.bind(metaControllers));
+    server.put('/meta/update/:id',metaControllers.updateMeta.bind(metaControllers));
+    server.delete('/meta/delete/:id',metaControllers.deleteMeta.bind(metaControllers));
 }
--- server/service/metaService.js
+++ server/service/metaService.js
@@ -1,39 +1,40 @@
-import { database } from "../database/database.js";
 import { metaModel } from "../model/metaModel.js";
 
 
-export const metaService = {
-    create: (metaData) => {
+export class metaService{
+    constructor(database){
+        this.database = database
+    }
+    async create(metaData) {
         const meta = metaModel.parse(metaData);
         try{
-        database.createMeta(meta);}
+        await this.database.createMeta(meta);}
         catch (error) {
             throw new Error('Erro ao criar meta: ' + error.message);
-            console.log(error);
         }
-    },
+    }
 
-    getMetas: (search) => {
+    async getMetas (search) {
         try{
-            const metas = database.getMetas(search)
+            const metas = await this.database.getMetas(search)
             return metas;
         }catch (e){
             throw new Error('Erro ao buscar metas: ' + e.message);
         }
-    },
+    }
 
-    updateMeta: (id,metaData) => {
+    async updateMeta (id,metaData) {
         try{
             const meta = metaModel.parse(metaData);
-            database.updateMeta(id,meta)
+            await this.database.updateMeta(id,meta)
         }catch (e){
             throw new Error('Erro ao atualizar meta: ' + e.message);
         }
-    },
+    }
 
-    deleteMeta: (id) => {
+    async deleteMeta (id) {
         try{
-            database.deleteMeta(id)
+            await this.database.deleteMeta(id)
         }
         catch (e){
             throw new Error("Erro ao deletar meta: " + e.message)
